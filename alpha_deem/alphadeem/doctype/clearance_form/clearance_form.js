7bb9367ceec2f9561da1a2fb994c100dd6c7ac90
AhmedRamzi22 <ahmed.ramzi222@gmail.com>
2023-08-15 12:27:40 +0300

    clearance

--- alpha_deem/alphadeem/doctype/clearance_form/clearance_form.js
+++ alpha_deem/alphadeem/doctype/clearance_form/clearance_form.js
@@ -2,7 +2,25 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Clearance Form', {
+
 	// refresh: function(frm) {
 
 	// }
+	employee:function(frm){
+		
+	frappe.db.get_doc('Department managers', 'Department managers')
+		.then(doc => {
+			
+				
+			frm.set_value('finance_name', doc.finance_manager)
+			frm.set_value('hr_name', doc.hr_manager)
+			frm.set_value('movement_department_name', doc.movement_manager)
+			frm.set_value('top_management_name', doc.top_manager)
+			frm.set_value('name_itdep', doc.it_manager)
+			frm.set_value('customer_accountant_name', doc.customer_accountant)
+			frm.set_value('warehouses_name', doc.warehouses_manager)
+			
+			
+		})
+	}
 });
